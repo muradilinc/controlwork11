f992c838455ea1ef00dc8d419aaa6c83f8ed375e
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-03-02 13:12:05 +0600

    init frontend and init redux, persist, router

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,37 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const usersPersistConfig = {
+  key: "shop:users",
+  storage: storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  // users: persistReducer(usersPersistConfig, usersReducer),
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+);
